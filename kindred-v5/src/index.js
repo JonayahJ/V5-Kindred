3124767e4069434b4440cca4c57b8c0cccad4c6d
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-03-28 11:29:18 -0400

    initial commit

---
+++ kindred-v5/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      Hello world!
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
+
+export default App
