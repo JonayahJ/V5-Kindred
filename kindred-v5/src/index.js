5b3c33fa787ea290d626e2c28b8ba12694db7631
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-03-28 14:10:54 -0400

    finished the clans.json file

--- kindred-v5/src/index.js
+++ kindred-v5/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'fontsource-roboto';
+import { Container } from '@material-ui/core';
 
 const App = () => {
   return (
     <div>
-      Hello world!
+      <Container>
+        <h1>
+          Hello neonates and fellow kindred!
+        </h1>
+        <p>
+          Create your new character sheet here.
+        </p>
+      </Container>
     </div>
   )
 }
