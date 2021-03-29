cc6836899fc2f550732e4a975324e5a70f26c519
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-03-29 14:15:33 -0400

    added build for netlify

---
+++ kindred/build/static/js/main.2d4cbdcc.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpkindred=this.webpackJsonpkindred||[]).push([[0],{14:function(e,r,n){"use strict";n.r(r);n(0);var t=n(8),c=n.n(t),d=(n(20),n(26)),s=n(2),i=function(){return Object(s.jsx)("div",{children:Object(s.jsxs)(d.a,{children:[Object(s.jsx)("h1",{children:"Hello neonates and fellow kindred!"}),Object(s.jsx)("p",{children:"Create your new character sheet here."})]})})};c.a.render(Object(s.jsx)(i,{}),document.querySelector("#root")),r.default=i}},[[14,1,2]]]);
+//# sourceMappingURL=main.2d4cbdcc.chunk.js.map
